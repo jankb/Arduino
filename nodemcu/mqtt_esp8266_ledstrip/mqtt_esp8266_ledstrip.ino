aa72943f80d85bf3822c8931ec78b0653374e775
jankb <jan.kristian.bueie@gmail.com>
2020-04-27 01:08:59 +0200

    Functioning WS2812b with MQTT control

--- nodemcu/mqtt_esp8266_ledstrip/mqtt_esp8266_ledstrip.ino
+++ nodemcu/mqtt_esp8266_ledstrip/mqtt_esp8266_ledstrip.ino
@@ -1,22 +1,22 @@
 #include <dummy.h>
+#include <NeoPixelBus.h>
 
-#include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
-#define PIN 6
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);
+const uint16_t PixelCount = 30;
+const uint8_t PixelPin = 2;  // make sure to set this to the correct pin, ignored for Esp8266
+#define colorSaturation 128
+NeoPixelBus<NeoGrbFeature, NeoEsp8266Dma800KbpsMethod> strip(PixelCount, PixelPin);
+
 
 #define TCP_MSS whatever
 #define LWIP_IPV6 whatever
 #define LWIP_FEATURES whatever
 #define LWIP_OPEN_SRC whatever
-#define RED_LED 16
 
-#include "DHT.h"
-#define DHTPIN 4     // what digital pin the DHT22 is conected to
-#define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
+
 
 #include <ESP8266WiFi.h>
 #include "Adafruit_MQTT.h"
@@ -44,32 +44,24 @@ WiFiClient client;
 
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
-
-/****************************** Feeds ***************************************/
-
-// Setup a feed called 'photocell' for publishing.
-// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/v3/photocell");
-
-Adafruit_MQTT_Publish hum = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/v3/hum");
-Adafruit_MQTT_Publish tmp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/v3/tmp");
-
 // Setup a feed called 'onoff' for subscribing to changes.
 Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/v3/onoff");
 
-Adafruit_MQTT_Subscribe redled = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/v3/redled");
+Adafruit_MQTT_Subscribe redLevel   = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/v3/redLevel");
+Adafruit_MQTT_Subscribe greenLevel = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/v3/greenLevel");
+Adafruit_MQTT_Subscribe blueLevel  = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/v3/blueLevel");
 
-/*************************** Sketch Code ************************************/
+Adafruit_MQTT_Subscribe rgbLevel  = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/v3/rgb");
 
-// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
-// for some reason (only affects ESP8266, likely an arduino-builder bug).
+
+
 void MQTT_connect();
 
-DHT dht(DHTPIN, DHTTYPE);
+
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(RED_LED, OUTPUT);
+  
   
   Serial.begin(115200);
   delay(10);
@@ -77,44 +69,61 @@ void setup() {
   Serial.print("Sketch:   ");   Serial.println(__FILE__);
   Serial.print("Uploaded: ");   Serial.println(__DATE__);
 
-  Serial.println(F("Adafruit MQTT demo"));
+  Serial.println(F("NeoPixelBus/MQTT demo"));
 
   // Connect to WiFi access point.
-  Serial.println(); Serial.println();
+  Serial.println(); 
+  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
 
   WiFi.begin(WLAN_SSID, WLAN_PASS);
   while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(RED_LED, LOW);
     delay(500);
     Serial.print(".");
-    digitalWrite(RED_LED, HIGH);
   }
   Serial.println();
 
-  dht.begin();
+  
   digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(RED_LED, HIGH);
+  
   Serial.println("WiFi connected");
   Serial.println("IP address: "); Serial.println(WiFi.localIP());
 
-  // Setup MQTT subscription for onoff feed.
+  
   mqtt.subscribe(&onoffbutton);
-  mqtt.subscribe(&redled);
+  mqtt.subscribe(&redLevel);
+  mqtt.subscribe(&greenLevel);
+  mqtt.subscribe(&blueLevel);
+  mqtt.subscribe(&rgbLevel);
 
 
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip.Begin();
+  strip.Show(); // Initialize all pixels to 'off'
   
 }
 
-uint32_t x=0;
 
+uint8_t red_level   = 0;
+uint8_t green_level = 0;
+uint8_t blue_level  = 0;
+
+
+bool isStripOn = true;
+
+void colorWipe(RgbColor c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.PixelCount(); i++) {
+    strip.SetPixelColor(i, c);
+    strip.Show();
+    Serial.print('s');
+    delay(wait);
+  }
+  Serial.println('*');
+}
 
 void loop() {
 
-     
+  
   // Ensure the connection to the MQTT server is alive (this will make the first
   // connection and automatically reconnect when disconnected).  See the MQTT_connect
   // function definition further below.
@@ -124,67 +133,63 @@ void loop() {
   // try to spend your time here
 
   Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(5000))) {
-    if (subscription == &onoffbutton) {
+  while ((subscription = mqtt.readSubscription(5000))) 
+  {
+    if (subscription == &onoffbutton) 
+    {
       Serial.print(F("Got: "));
       Serial.println((char *)onoffbutton.lastread);
       if (strcmp((char *)onoffbutton.lastread, "ON") == 0)
        {
          digitalWrite(LED_BUILTIN, LOW); 
+         isStripOn = true;
        }
        else
        {
          digitalWrite(LED_BUILTIN, HIGH); 
+         colorWipe(RgbColor(0, 0, 0), 1);
+         isStripOn = false;
        }
     }
-     if (subscription == &redled) {
-      Serial.print(F("Redled Got: "));
-      Serial.println((char *)redled.lastread);
-      if (strcmp((char *)redled.lastread, "ON") == 0)
-       {
-         digitalWrite(RED_LED, LOW);
-       }
-       else
-       {
-         digitalWrite(RED_LED, HIGH);
-       }
-    }
-  }
-/*
-  // Now we can publish stuff!
-  Serial.print(F("\nSending photocell val "));
-  Serial.print(x);
-  Serial.print("...");
-  if (! photocell.publish(x++)) {
-    Serial.println(F("Failed"));
-  } else {
-    Serial.println(F("OK!"));
-  }
-*/
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-
-    float h = dht.readHumidity();
-    if (isnan(h) == false)
+    if (subscription == &redLevel) 
     {
-      Serial.print("Humidity : ");
-      Serial.println(h);
-      hum.publish(h);
+       Serial.print(F("RED Got: "));
+       red_level = atoi((char *)redLevel.lastread);
+      Serial.println(red_level);
+      
     }
-
-    float t = dht.readTemperature();
-    if (isnan(t) == false)
+    if (subscription == &greenLevel) 
     {
-      Serial.print("Temperature : ");
-      Serial.println(t);
-      tmp.publish(t);
+       Serial.print(F("GREEN Got: "));
+       green_level = atoi((char *)greenLevel.lastread);
+      Serial.println(green_level);
+      
     }
-  // ping the server to keep the mqtt connection alive
-  // NOT required if you are publishing once every KEEPALIVE seconds
-  /*
-  if(! mqtt.ping()) {
-    mqtt.disconnect();
+    if (subscription == &blueLevel) 
+    {
+       Serial.print(F("BLUE Got: "));
+       blue_level = atoi((char *)blueLevel.lastread);
+      Serial.println(blue_level);
+      
+    }
+     if (subscription == &rgbLevel) 
+    {
+       Serial.print(F("RGB Got: "));
+      Serial.println((char*)rgbLevel.lastread);
+      uint32_t inputv = atoi((char *)rgbLevel.lastread);
+      red_level = (inputv >> 16);
+      green_level = (inputv >> 8);
+      blue_level = (inputv);
+      
+      
+    }
+    if (isStripOn)
+    {
+      colorWipe(RgbColor(red_level, green_level, blue_level), 1);
+    }
   }
-  */
+  
+  
 }
 
 // Function to connect and reconnect as necessary to the MQTT server.
